82b881892e1cfc45ef867d42a6321e28ccb74b96
namhPhiyada <109198499+namhPhiyada@users.noreply.github.com>
2024-01-23 17:54:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,9 @@ if uploaded_file is not None:
     st.write("Names:")
     for name in unique_names:
         st.text(name)
-    
-    st.text(unique_names)
+
+    if unique_names == ['ดาราทอง (Darathong)']:
+        st.image(Image.open("data/images/Darathong.jpg"), caption='Original Image', use_column_width=True
+  
 
    
