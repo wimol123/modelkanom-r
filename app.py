d9b7e283139927e20a3cc45b0d12181aa802d970
namhPhiyada <109198499+namhPhiyada@users.noreply.github.com>
2024-01-23 16:32:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,32 @@
-import argparse
 import streamlit as st
-import io
-import os
-from PIL import Image
 import numpy as np
-import torch, json , cv2 , detect
-
+import cv2
+from PIL import Image
+import torch
+import detect
 
 st.title("YUMMY")
-
 st.write("Upload your Image...")
 
-#model = torch.hub.load('./yolov5', 'custom', path='./last.pt', source='local')
-#model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/last.pt', force_reload=True)
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/Model2.pt')
 
 uploaded_file = st.file_uploader("Choose .jpg pic ...", type="jpg")
 if uploaded_file is not None:
-  
-  file_bytes = np.asarray(bytearray(uploaded_file.read()))
-  image = cv2.imdecode(file_bytes, 1)
-
-  imgRGB = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
-  #st.image(imgRGB)
-
-  st.write("")
-  st.write("Detecting...")
-  result = model(imgRGB, size=600)
-  
-  detect_class = result.pandas().xyxy[0] 
-  
-  #labels, cord_thres = detect_class[:, :].numpy(), detect_class[:, :].numpy()
-  
-  #     xmin       ymin    xmax        ymax          confidence  class    name
-  #0  148.605362   0.0    1022.523743  818.618286    0.813045      2      turtle
-  
-  st.code(detect_class[['name']])
-  
-  
-  
-  #st.success(detect_class)
-  
-  outputpath = 'output.jpg'
-  
-  result.render()  # render bbox in image
-  for im in result.ims:
-      im_base64 = Image.fromarray(im)
-      im_base64.save(outputpath)
-      img_ = Image.open(outputpath)
-      st.image(img_, caption='Model Prediction(s)')
-
+    file_bytes = np.asarray(bytearray(uploaded_file.read()))
+    image = cv2.imdecode(file_bytes, 1)
+    imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # Display the uploaded image
+    st.image(imgRGB, caption='Uploaded Image', use_column_width=True)
+    
+    st.write("")
+    st.write("Detecting...")
+    result = model(imgRGB, size=600)
+    
+    # Set a confidence threshold (adjust as needed)
+    confidence_threshold = 0.5
+    detect_class = result.xyxy[0][result.xyxy[0][:, 4] > confidence_threshold]
+    
+    st.code(detect_class[:, [6, 0, 2, 1, 3, 4]], caption=['name', 'xmin', 'xmax', 'ymin', 'ymax', 'confidence'])
+    
+    st.image(Image.fromarray(result.ims[0]), caption='Model Prediction(s)', use_column_width=True)
