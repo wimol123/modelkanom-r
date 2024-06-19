cfef1f6cee01753077b32d064b142ea8a06b48b0
namhPhiyada <109198499+namhPhiyada@users.noreply.github.com>
2024-06-19 18:31:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.write("Upload your Image...")
 
 # model = torch.hub.load('./yolov5', 'custom', path='./last.pt', source='local')
 # model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/last.pt', force_reload=True)
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/Model2.pt')
+# model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/Model2.pt')
 
 uploaded_file = st.file_uploader("Choose .jpg pic ...", type="jpg")
 if uploaded_file is not None:
@@ -27,30 +27,30 @@ if uploaded_file is not None:
 
     st.write("")
     st.write("Detecting...")
-    result = model(imgRGB, size=600)
+    # result = model(imgRGB, size=600)
 
-    detect_class = result.pandas().xyxy[0]
+    # detect_class = result.pandas().xyxy[0]
 
-    detect_class['name'] = detect_class['name'].map({'Darathong': 'Darathong',
-                                                     'SaneCharn': 'SaneCharn',
-                                                     'ChorMuang': 'ChorMuang'})
+    # detect_class['name'] = detect_class['name'].map({'Darathong': 'Darathong',
+    #                                                  'SaneCharn': 'SaneCharn',
+    #                                                  'ChorMuang': 'ChorMuang'})
 
     # Get unique names
-    unique_names = detect_class['name'].unique()
+    # unique_names = detect_class['name'].unique()
 
     # Display the unique names without numbers
-    st.write("Names:")
-    for name in unique_names:
-        if name == 'Darathong':
-            st.text('TH: ดาราทอง  EN: Darathong')
-        elif name == 'SaneCharn':
-            st.text('TH: เสน่ห์จันทร์  EN: SaneCharn')
-        elif name == 'ChorMuang':
-            st.text('TH: ช่อม่วง  EN: ChorMuang')
+    # st.write("Names:")
+    # for name in unique_names:
+    #     if name == 'Darathong':
+    #         st.text('TH: ดาราทอง  EN: Darathong')
+    #     elif name == 'SaneCharn':
+    #         st.text('TH: เสน่ห์จันทร์  EN: SaneCharn')
+    #     elif name == 'ChorMuang':
+    #         st.text('TH: ช่อม่วง  EN: ChorMuang')
             
-        image_path = f"data/images/{name}.jpg"
-        if os.path.exists(image_path):
-            st.image(Image.open(image_path), caption='Original Image', use_column_width=True)
+    #     image_path = f"data/images/{name}.jpg"
+    #     if os.path.exists(image_path):
+    #         st.image(Image.open(image_path), caption='Original Image', use_column_width=True)
     
     # if unique_names == ['ดาราทอง (Darathong)']:
     #     st.image(Image.open("data/images/Darathong.jpg"), caption='Original Image', use_column_width=True)
